ab30b6c0fd1a54dbb28e49a689ce0c152cec3c53
Grant <gnb@pdx.edu>
2021-05-23 22:53:36 -0700

    Added functionality to reading spectogram from spectogram file

--- Spectogram.py
+++ Spectogram.py
@@ -40,11 +40,40 @@ class Spectogram():
                     f.write(str(complex_val.real) + ' ' + str(complex_val.imag) + ' ' + 
                     str(np.abs(complex_val)) + ' ' + str(np.angle(complex_val)) + '\n')
 
+    def load_spectogram_from_file(self, spectogram_file):
+        with open(spectogram_file) as f:
+            time_dim, freq_dim = [int(x) for x in f.readline().split()]
+            spectogram_matrix = np.zeros(shape=(freq_dim,time_dim))
+            for time in range(time_dim):
+                for freq in range(freq_dim):
+                    spectogram_matrix[freq, time] = f.readline().split()[2]
+        return spectogram_matrix
+
+    def real_spectogram_to_db_spectogram(self, spectogram):
+        return librosa.amplitude_to_db(spectogram, ref=np.max)
 
 
-    def display_spectogram(self, spectogram):
+    def display_complex_spectogram(self, spectogram):
         real_spectogram_db = librosa.amplitude_to_db(np.abs(spectogram), ref=np.max)
         plt.figure()
         librosa.display.specshow(real_spectogram_db)
         plt.colorbar()
         plt.show()
+
+    def display_real_spectogram(self, spectogram):
+        real_spectogram_db = self.real_spectogram_to_db_spectogram(spectogram)
+        plt.figure()
+        librosa.display.specshow(real_spectogram_db)
+        plt.colorbar()
+        plt.show()
+
+def file_test():
+    spectogram = Spectogram()
+    spec=spectogram.signal_to_spectogram('test_data/0/Medley-solos-DB_test-0_7f9c729c-396b-5cd7-f2ea-b137d8ee7222.wav')
+    spectogram.display_complex_spectogram(spec)
+    spectogram.signal_to_spectogram_file('test_data/0/Medley-solos-DB_test-0_7f9c729c-396b-5cd7-f2ea-b137d8ee7222.wav', 'spec')
+    spec=spectogram.load_spectogram_from_file('spec')
+    spectogram.display_real_spectogram(spec)
+
+if __name__ == '__main__':
+    file_test()
