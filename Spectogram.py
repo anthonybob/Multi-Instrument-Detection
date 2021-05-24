fe0ade1c1b1473cfeb2a9306dab395d91c70a70c
Grant <gnb@pdx.edu>
2021-05-24 10:07:38 -0700

    Use more reasonable stft parameters

--- Spectogram.py
+++ Spectogram.py
@@ -8,7 +8,7 @@ class Spectogram():
     #Window length is the size of the window
     #Window padding is number of zeros to pad audio after window is applied
     #Hop length is the number of audio samples between each STFT output
-    def __init__(self, window_length=2048, window_padding=0, hop_length=None, window=scipy.signal.windows.hann):
+    def __init__(self, window_length= 512, window_padding=0, hop_length=256, window=scipy.signal.windows.hann):
         self.window_length = window_length
         self.window_padding = window_padding
         self.window = window
@@ -22,6 +22,10 @@ class Spectogram():
         signal, sampling_rate = librosa.load(wav_file)
         spectogram_matrix = librosa.stft(signal, n_fft = self.window_length + self.window_padding, hop_length=self.hop_length)
         return spectogram_matrix
+
+    @staticmethod
+    def float_to_string(real_num):
+        return "{:.2f}".format(real_num)
 
     #The spectogram file output is as follows:
     #The first line has two integers. The first number is the number of time samples, and the second number is the number of 
@@ -38,8 +42,8 @@ class Spectogram():
             for time_val in range(spectogram_matrix.shape[1]):
                 for frequency_bin in range(spectogram_matrix.shape[0]):
                     complex_val = spectogram_matrix[frequency_bin][time_val]
-                    f.write(str(complex_val.real) + ' ' + str(complex_val.imag) + ' ' + 
-                    str(np.abs(complex_val)) + ' ' + str(np.angle(complex_val)) + '\n')
+                    f.write(Spectogram.float_to_string(complex_val.real) + ' ' + Spectogram.float_to_string(complex_val.imag) + ' ' + 
+                    Spectogram.float_to_string(np.abs(complex_val)) + ' ' + Spectogram.float_to_string(np.angle(complex_val)) + '\n')
     @staticmethod
     def load_spectogram_from_file(spectogram_file):
         with open(spectogram_file) as f:
@@ -73,6 +77,7 @@ class Spectogram():
 def file_test():
     spectogram = Spectogram()
     spec=spectogram.signal_to_spectogram('test_data/0/Medley-solos-DB_test-0_7f9c729c-396b-5cd7-f2ea-b137d8ee7222.wav')
+    print(spec.shape[0], spec.shape[1])
     Spectogram.display_complex_spectogram(spec)
     spectogram.signal_to_spectogram_file('test_data/0/Medley-solos-DB_test-0_7f9c729c-396b-5cd7-f2ea-b137d8ee7222.wav', 'spec')
     spec=Spectogram.load_spectogram_from_file('spec')
@@ -93,4 +98,4 @@ def convert_signals_to_spectograms():
     print('Done Converting!')
         
 if __name__ == '__main__':
-    convert_signals_to_spectograms()
+    file_test()
