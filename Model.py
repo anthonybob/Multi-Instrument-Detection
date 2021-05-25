a066b99a855038c4a047ae110df3fb8d4d873af1
Grant <gnb@pdx.edu>
2021-05-24 22:04:01 -0700

    Implemented FC model with train/test output

--- Model.py
+++ Model.py
@@ -1,41 +1,46 @@
 import torch
+import torch.utils.data
 import numpy as np
 from Spectogram import Spectogram
 import os
 class Model:
 
-    def __init__(self, model, loader, optimizer, device, criterion):
+    def __init__(self, model, trainloader, testloader, optimizer, device, criterion, num_inputs):
         self.model = model
-        self.loader = loader
+        self.trainloader = trainloader
+        self.testloader = testloader
         self.optimizer = optimizer
         self.device = device
         self.criterion = criterion
+        self.num_inputs = num_inputs
+        self.model = self.model.to(device)
 
     def train(self):
         total_loss = 0
-        for _, batch in enumerate(loader, 0):
+        for _, batch in enumerate(self.trainloader, 0):
 
             inputs, labels = batch
             inputs = inputs.to(self.device)
             labels = labels.to(self.device)
 
             self.optimizer.zero_grad()
-            output = self.model(inputs)
+            output = self.model(inputs.float(), self.num_inputs)
             loss = self.criterion(output, labels)
             total_loss += loss.item()
             loss.backward()
-            optimizer.step()
-        return total_loss / len(loader)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
+            self.optimizer.step()
+        return total_loss / len(self.trainloader)
 
     def test(self):
         total = 0
         correct = 0
         with torch.no_grad():
-            for batch in self.loader:
+            for batch in self.testloader:
                 inputs, labels = batch
                 inputs = inputs.to(self.device)
                 labels = labels.to(self.device)
-                output = model(inputs)
+                output = self.model(inputs.float(), self.num_inputs)
                 output = output.to('cpu')
                 labels = labels.to('cpu')
                 for i in range(len(inputs)):
@@ -46,7 +51,7 @@ class Model:
                     total += 1
         return correct / total
 
-class Spectogram_DataLoader(torch.utils.data.DataLoader):
+class Spectogram_DataLoader(torch.utils.data.Dataset):
     def __init__(self, spectogram_files):
         self.length = len(spectogram_files)
         self.spectograms = []
@@ -81,14 +86,14 @@ class FC(torch.nn.Module):
         x = self.layers[-1](x)
         return x
 
-def loader(loader_type):
+def dataset(data_type):
     files = []
     prefix = None
-    if loader_type == 'train':
+    if data_type == 'train':
         prefix = 'training_data/'
 
-    elif loader_type == 'test':
-        prefix = 'test_data'
+    elif data_type == 'test':
+        prefix = 'test_data/'
 
     else:
         return None
@@ -98,11 +103,26 @@ def loader(loader_type):
         if 'spec' not in label_dir:
             continue
         path = prefix + label_dir + '/'
-        for wav_file in os.listdir(path):
-            files.append(spectogram_file, int(prefix[4:]))
+        for spectogram_file in os.listdir(path):
+            files.append((path + spectogram_file, int(label_dir[4:])))
 
-        return Spectogram_DataLoader(files)
+    return Spectogram_DataLoader(files)
 
         
+def main():
+    trainset = dataset('train')
+    testset = dataset('test')
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size = 32, shuffle = True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size = 32, shuffle = False)
+    fc_model = FC(torch.relu, [1000, 1000, 1000], 22500, 8)
+    optimizer = torch.optim.SGD(fc_model.parameters(), lr = 0.001, momentum = 0.001)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    criterion = torch.nn.CrossEntropyLoss()
+    model = Model(fc_model, trainloader, testloader, optimizer, device, criterion, 22500)
+    for epoch in range(10000):
+        print('Epoch ' + str(epoch))
+        print(model.train())
+        print(model.test())
+        torch.save(fc_model.state_dict(), 'model')
 
-loader('train')
+main()
